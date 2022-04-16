652114ba2e965611f86fed37295615510281e4d3
Surin Lee <leesurin99@gmail.com>
2022-04-16 15:59:53 +0900

    (fix)deploy setting update

--- soul_prj/settings/deploy.py
+++ soul_prj/settings/deploy.py
@@ -1,10 +1,9 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 # AWS Setting
 AWS_REGION = 'ap-northeast-2'
 AWS_STORAGE_BUCKET_NAME = '7th-team1-soul'
-AWS_QUERYSTRING_AUTH = False
 AWS_S3_HOST = 's3.%s.amazonaws.com' % AWS_REGION
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
@@ -12,7 +11,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Static Setting
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_STORAGE = 'soul_prj.storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 ALLOWED_HOSTS = ['*']
 
@@ -27,6 +26,7 @@ DATABASES = {
 
 THIRD_PARTY_APPS += [
     'corsheaders',
+    'storages',
 ]
 
 MIDDLEWARE +=[
